d06b93b9c546f7b3640617d6613f977b1641cfd7
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:06:36 +0530

    adding TriOutLineStepView class

--- trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
+++ trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
@@ -36,3 +36,21 @@ fun Canvas.drawTOLSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TriOutLineStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
