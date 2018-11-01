3992fb76ed53b1b408dfa36146f4a8d5444a2147
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:42:59 +0530

    adding static create method

--- trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
+++ trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
@@ -4,6 +4,7 @@ package com.anwesh.uiprojects.trioutlinestepview
  * Created by anweshmishra on 01/11/18.
  */
 
+import android.app.Activity
 import android.view.View
 import android.view.MotionEvent
 import android.content.Context
@@ -198,6 +199,14 @@ class TriOutLineStepView(ctx : Context) : View(ctx) {
             tols.startUpdating {
                 animator.start()
             }
+        }
+    }
+
+    companion object {
+        fun create(activity : Activity) : TriOutLineStepView {
+            val view : TriOutLineStepView = TriOutLineStepView(activity)
+            activity.setContentView(view)
+            return view 
         }
     }
 }
