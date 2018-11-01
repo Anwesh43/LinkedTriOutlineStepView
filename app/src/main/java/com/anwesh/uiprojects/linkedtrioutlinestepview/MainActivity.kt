193a8841133e777d07baadde1876c88a31dd6e49
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:45:20 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtrioutlinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.trioutlinestepview.TriOutLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriOutLineStepView.create(this)
     }
 }
