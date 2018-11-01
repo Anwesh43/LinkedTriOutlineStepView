9f038819ecf9cecf57d2df49c01f178ba9b441e4
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:24:18 +0530

    adding TOLSNode class

--- trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
+++ trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
@@ -104,4 +104,49 @@ class TriOutLineStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TOLSNode(var i : Int, val state : State = State()) {
+
+        private var next : TOLSNode? = null
+
+        private var prev : TOLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TOLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTOLSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TOLSNode {
+            var curr : TOLSNode? = this.prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
