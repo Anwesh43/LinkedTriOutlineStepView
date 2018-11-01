c0a0dc455c5452fe552b73061dfa077adbd71161
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:28:52 +0530

    adding TriOutLineStep class

--- trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
+++ trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
@@ -149,4 +149,30 @@ class TriOutLineStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriOutLineStep(var i : Int) {
+
+        private val root : TOLSNode = TOLSNode(0)
+
+        private var curr : TOLSNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
