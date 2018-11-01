47abbaeb6e0bffeb132cf141b584b4b688870a7b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 10:50:22 +0530

    adding libraries and defining constants

--- trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
+++ trioutlinestepview/src/main/java/com/anwesh/uiprojects/trioutlinestepview/TriOutLineStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.trioutlinestepview
 /**
  * Created by anweshmishra on 01/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val sides : Int = 3
