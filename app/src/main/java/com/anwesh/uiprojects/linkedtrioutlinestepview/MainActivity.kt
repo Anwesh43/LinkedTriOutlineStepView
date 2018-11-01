c8a67926889b1a7dad453f5dc63d057a5f42baee
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 12:04:11 +0530

    adding AnimationListener in  Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedtrioutlinestepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.trioutlinestepview.TriOutLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TriOutLineStepView.create(this)
+        val view : TriOutLineStepView = TriOutLineStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("${it} is complete")}, {createToast("${it} is reset")})
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_LONG).show()
     }
 }
 
