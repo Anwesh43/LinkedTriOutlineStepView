f17b284da4258431fca4c6b55d1152ed9a4b66e1
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 11:46:21 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrioutlinestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtrioutlinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.trioutlinestepview.TriOutLineStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriOutLineStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
